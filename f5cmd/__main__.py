979dcf0882fecd9b0d005cfe3d023e445364d867
wcollins <william.levi.collins@gmail.com>
2017-09-04 15:28:57 -0400

    Initial f5cmd commit

---
+++ f5cmd/__main__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from .f5cmd import main
+
+main()
---
+++ run.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+''' Run directly from source tree '''
+
+from f5cmd.f5cmd import main
+
+if __name__ == '__main__':
+
+    main()
